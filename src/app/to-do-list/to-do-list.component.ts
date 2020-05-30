a3c25b9bc708bd98b3ea2b9a4c0b6dd3ebedb2f0
Renny Mittal <rennymittal1234@gmail.com>
2020-05-30 22:00:12 +0530

    half page done

---
+++ src/app/to-do-list/to-do-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.css']
+})
+export class ToDoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  users=[1,2,3,5];
+  
+
+}
