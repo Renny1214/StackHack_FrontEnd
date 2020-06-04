ceb6196dc294d236eac267e4d3373370f5efd20a
Rishabh <rishabhmalhotra091@gmail.com>
2020-06-04 13:04:12 +0530

    add function completed

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-to-do-list',
@@ -7,11 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDoListComponent implements OnInit {
 
-  constructor() { }
+  priority;
 
-  ngOnInit() {
+  showLoading = false;
+
+  data = {
+    category: null,
+    task: null,
+    priority: null,
+    completed: false,
+    date: {
+      current: null,
+      due: null
+    }
+  };
+
+  constructor(private httpCLient: HttpClient) { }
+
+  ngOnInit() {}
+
+  enterCategory(event) {
+    this.data.category = event.target.value;
   }
-  users=[1,2,3,5];
-  
+  enterEvent(event) {
+    this.data.task = event.target.value;
+  }
+  enterDate(event) {
+    this.data.date.due = Date.parse(event.target.value);
+    this.data.date.current = Date.now();
 
+    console.log(this.data);
+  }
+  enterPriority(event) {
+    this.priority = event.target.value;
+  }
+
+  add() {
+    this.showLoading = true;
+    const arr = ['high', 'medium', 'low'];
+    const index = arr.indexOf(this.priority);
+
+    const url = 'https://stackhack-node.herokuapp.com/add';
+
+    if(index !== -1) {
+      this.data.priority = index + 1;
+      
+      this.httpCLient.post(url, this.data).subscribe((res) => {
+        this.showLoading = false;
+        console.log(res);
+        alert('Task Added');
+      },
+      error => {
+        this.showLoading = false;
+        alert('Error in adding task');
+      })
+    }
+    else {
+      this.showLoading = false;
+      alert('Enter correct priority');
+    }
+  }
 }
